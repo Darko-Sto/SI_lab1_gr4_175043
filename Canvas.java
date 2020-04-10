3a3bea4743cc8575500deefb96969804f26c9de4
Darko-Sto <darkostojanovski3@gmail.com>
2020-04-10 20:20:18 +0200

    Canvas commit

---
+++ Canvas.java
@@ -0,0 +1,17 @@
+[Rayman2]
+GLI_DllFile=GliDX6
+GLI_Dll=DirectX6
+GLI_Driver=\\.\DISPLAY1
+GLI_Device=Direct3D HAL
+GLI_Mode=1 - 1024 x 768 x 16
+Language=English
+ParticuleRate=High
+
+[Rayman2]
+GLI_DllFile=GliVd1
+GLI_Dll=Glide2
+GLI_Driver=
+GLI_Device=
+GLI_Mode=1 - 1024 x 768 x 16
+Language=English
+ParticuleRate=High
